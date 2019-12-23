2fffa0dfed9b98c75fb9ab04467a5c578417e334
robert.lo <robert.lo@amtran.com.tw>
2019-12-23 17:49:55 +0800

    [import] - To add controls for FAN on the server side.

--- Firebase Functions/index.js
+++ Firebase Functions/index.js
@@ -13,9 +13,34 @@ exports.relay = functions.https.onRequest((request, response) => {
     //  Dialogflow sends API V2 json file to Firebase Functions via HTTP POST request.
     //  https://us-central1-MY_PROJECT.cloudfunctions.net/relay
     let params = request.body.queryResult.parameters;
+    let outstr = "Fan Relay is controlled succesfully.";
 
     console.log("This message will be printed on /functions/logs on Firebase.");
     console.log(params);
+
+
+    var fanspeed = parseInt(params['percentage']);
+
+    console.log("Param 1 = " + params['activate']);
+    console.log("Param 2 = " + params['number']);
+    console.log("Param 3 = " + params['deactivate']);
+    console.log("fanspeed = " + fanspeed);
+
+
+    if(params['number'] == 1){
+        outstr = "Fan 1 is controlled";
+    }
+    else if(params['number'] == 2){
+        outstr = "Fan 2 is controlled";
+    }
+    else{
+        outstr = "The Fan is ready for control"
+    }
+
+    if(fanspeed > 0){
+        outstr = outstr + " In Fan Speed " + params['percentage'];
+    }
+    
 
     database.ref().set(params);
 
@@ -29,7 +54,7 @@ exports.relay = functions.https.onRequest((request, response) => {
                         "items": [
                             {
                                 "simpleResponse": {
-                                    "textToSpeech": "Relay is controlled succesfully."
+                                    "textToSpeech": outstr
                                 }
                             }
                         ]
