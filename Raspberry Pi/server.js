a88ae809db9d8f53fc174990701b779b339979ad
robert.lo <robert.lo@amtran.com.tw>
2019-12-24 14:36:46 +0800

    [import] - To modify Raspberry Pi GPIO for FAN control

--- Raspberry Pi/server.js
+++ Raspberry Pi/server.js
@@ -5,13 +5,15 @@ var fs = require('fs');
 
 // To define extra GPIO for LED display
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var LED1 = new Gpio(5, 'out');
-var LED2 = new Gpio(6, 'out');
-var LED3 = new Gpio(21, 'out');
+var ONOFF = new Gpio(26, 'out');
+var SPEED1 = new Gpio(5, 'out');
+var SPEED2 = new Gpio(6, 'out');
+var SPEED3 = new Gpio(21, 'out');
 
 const ACTIVATE = "activate";
 const DEACTIVATE = "deactivate";
 const RELAY = "number";
+const FANSPEED = 'fanspeed';
 
 const PATH_TO_RELAYS = {
 	1: "/sys/class/gpio/gpio26/value",
@@ -43,25 +45,49 @@ ref.on('value', function (snapshot) {
 
 	console.log(entitiesFromAssistant);
 
-	Object.entries(PATH_TO_RELAYS).forEach(([relay, path]) => {
-		if (relay == entitiesFromAssistant[RELAY]) {
-			if (entitiesFromAssistant[ACTIVATE]) {
-				LED1.writeSync(1);
-				LED2.writeSync(0);
-				LED3.writeSync(1);
+	if(entitiesFromAssistant[ACTIVATE] == '1'){
+		ONOFF.writeSync(1);
+	}
+	else if(entitiesFromAssistant[DEACTIVATE] == '0'){
+		ONOFF.writeSync(0);
+	}
+	else if(entitiesFromAssistant[FANSPEED] == '1'){
+		SPEED1.writeSync(1);
+		SPEED2.writeSync(0);
+		SPEED3.writeSync(0);
+	}
+	else if(entitiesFromAssistant[FANSPEED] == '2'){
+		SPEED1.writeSync(0);
+		SPEED2.writeSync(1);
+		SPEED3.writeSync(0);
+	}
+	else if(entitiesFromAssistant[FANSPEED] == '3'){
+		SPEED1.writeSync(0);
+		SPEED2.writeSync(0);
+		SPEED3.writeSync(1);
+	}
 
-				fs.writeFileSync(path, entitiesFromAssistant[ACTIVATE], function (err) {
-					if (err) throw err;
-				}); // fs.writeFileSync
-			} else {
-				LED1.writeSync(0);
-				LED2.writeSync(1);
-				LED3.writeSync(0);
 
-				fs.writeFileSync(path, entitiesFromAssistant[DEACTIVATE], function (err) {
-					if (err) throw err;
-				}); // fs.writeFileSync
-			} // else
-		} // if (relay == entitiesFromAssistant[RELAY])
-	}); // Object.entries(PATH_TO_RELAYS).forEach
+	// Object.entries(PATH_TO_RELAYS).forEach(([relay, path]) => {
+	// 	//if (relay == entitiesFromAssistant[RELAY]) {
+	// 		if (entitiesFromAssistant[ACTIVATE]) {
+	// 			LED1.writeSync(1);
+	// 			LED2.writeSync(0);
+	// 			LED3.writeSync(1);
+
+	// 			fs.writeFileSync(path, entitiesFromAssistant[ACTIVATE], function (err) {
+	// 				if (err) throw err;
+	// 			}); // fs.writeFileSync
+	// 		} else {
+	// 			LED1.writeSync(0);
+	// 			LED2.writeSync(1);
+	// 			LED3.writeSync(0);
+
+	// 			fs.writeFileSync(path, entitiesFromAssistant[DEACTIVATE], function (err) {
+	// 				if (err) throw err;
+	// 			}); // fs.writeFileSync
+	// 		} // else
+	// 	//} // if (relay == entitiesFromAssistant[RELAY])
+	// }); // Object.entries(PATH_TO_RELAYS).forEach
+
 }); // ref.on('value', function (snapshot)
