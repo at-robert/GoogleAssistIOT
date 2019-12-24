0eaad0f0588b6c4db869c1ccb3c4e146b87fb312
robert.lo <robert.lo@amtran.com.tw>
2019-12-24 19:55:53 +0800

    [import] - To add database access functions and improve FAN control functions

--- Firebase Functions/index.js
+++ Firebase Functions/index.js
@@ -8,6 +8,27 @@ admin.initializeApp(functions.config().firebase);
 
 var database = admin.database();
 
+// To read out database values
+var activate_db;
+var deactivate_db;
+var fanspeed_db;
+database.ref('/activate').on('value',e=>{
+    activate_db = e.val();
+    activate_db = parseInt(activate_db);
+    console.log("activate_db = " + activate_db);
+});
+database.ref('/deactivate').on('value',e=>{
+    deactivate_db = e.val();
+    deactivate_db = parseInt(deactivate_db);
+    console.log("deactivate_db = " + deactivate_db);
+});
+database.ref('/fanspeed').on('value',e=>{
+    fanspeed_db = e.val();
+    fanspeed_db = parseInt(fanspeed_db);
+    console.log("fanspeed_db = " + fanspeed_db);
+});
+// -- END read out database values
+
 exports.relay = functions.https.onRequest((request, response) => {
 
     //  Dialogflow sends API V2 json file to Firebase Functions via HTTP POST request.
@@ -18,7 +39,7 @@ exports.relay = functions.https.onRequest((request, response) => {
     console.log("This message will be printed on /functions/logs on Firebase.");
     console.log(params);
 
-
+    // Fan Control Logic ------
     var fanspeed = parseInt(params['fanspeed']);
     var activate = parseInt(params['activate']);
     var deactivate = parseInt(params['deactivate']);
@@ -28,29 +49,55 @@ exports.relay = functions.https.onRequest((request, response) => {
     console.log("deactivate = " + params['deactivate']);
     console.log("fanspeed = " + fanspeed);
 
+    // To restore database value
+    // params['activate'] = activate_db;
+    // params['deactivate'] = deactivate_db;
+    // params['fanspeed'] = fanspeed_db;
+    // --- END
+
     if(activate == 1){
-        outstr = "電扇開始運轉";
+        if(activate_db == 1){
+            outstr = "電扇已經在運轉";
+        }
+        else{
+            outstr = "電扇開始運轉";
+            database.ref('/activate').set('1');
+            database.ref('/deactivate').set('');
+        }
+            
     }
     else if(deactivate == 0){
-        outstr = "電扇停止運轉";
+        if(deactivate_db == 0){
+            outstr = "電扇已經停止運轉";
+        }
+        else{
+            outstr = "電扇停止運轉";
+            database.ref('/activate').set('');
+            database.ref('/deactivate').set('0');
+        }
     }
     else if(fanspeed == 1){
         outstr = "電扇設定為風速 1 ";
+        database.ref('/fanspeed').set(1);
     }
     else if(fanspeed == 2){
         outstr = "電扇設定為風速 2";
+        database.ref('/fanspeed').set(2);
     }
     else if(fanspeed == 3){
-            outstr = "電扇設定為風速 3";
+        outstr = "電扇設定為風速 3";
+        database.ref('/fanspeed').set(3);
     }
     else if(number > 3 ){
         outstr = "此電扇目前沒有支援風速 ＝ " + number;
     }
     else{
         outstr = "電扇已經可以控制了"
+        database.ref().set(params);
     }
+    // Fan Control Logic END ------
 
-    database.ref().set(params);
+    
 
     //  API V2 needs below response format.
     response.send(
