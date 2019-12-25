6ac055a448da2d774cf60519e0e266eb6b97aa6d
robert.lo <robert.lo@amtran.com.tw>
2019-12-25 12:18:29 +0800

    [import] - To add speed change functions and improve user experiences

--- Firebase Functions/index.js
+++ Firebase Functions/index.js
@@ -44,6 +44,7 @@ exports.relay = functions.https.onRequest((request, response) => {
     var activate = parseInt(params['activate']);
     var deactivate = parseInt(params['deactivate']);
     var number = parseInt(params['number']);
+    var speedchange = parseInt(params['speedchange']);
 
     console.log("activate = " + params['activate']);
     console.log("deactivate = " + params['deactivate']);
@@ -77,23 +78,88 @@ exports.relay = functions.https.onRequest((request, response) => {
         }
     }
     else if(fanspeed == 1){
-        outstr = "電扇設定為風速 1 ";
-        database.ref('/fanspeed').set(1);
+        if(deactivate_db == 0){
+            outstr = "電扇已經停止運轉,開啟電扇並設定為風速1";
+            database.ref('/activate').set('1');
+            database.ref('/deactivate').set('');
+            database.ref('/fanspeed').set(1);
+        }
+        else{
+            outstr = "電扇設定為風速 1 ";
+            database.ref('/fanspeed').set(1);
+        }
     }
     else if(fanspeed == 2){
-        outstr = "電扇設定為風速 2";
-        database.ref('/fanspeed').set(2);
+        if(deactivate_db == 0){
+            outstr = "電扇已經停止運轉,開啟電扇並設定為風速2";
+            database.ref('/activate').set('2');
+            database.ref('/deactivate').set('');
+            database.ref('/fanspeed').set(2);
+        }
+        else{
+            outstr = "電扇設定為風速 2 ";
+            database.ref('/fanspeed').set(2);
+        }
     }
     else if(fanspeed == 3){
-        outstr = "電扇設定為風速 3";
-        database.ref('/fanspeed').set(3);
+        if(deactivate_db == 0){
+            outstr = "電扇已經停止運轉,開啟電扇並設定為風速3";
+            database.ref('/activate').set('3');
+            database.ref('/deactivate').set('');
+            database.ref('/fanspeed').set(3);
+        }
+        else{
+            outstr = "電扇設定為風速 3 ";
+            database.ref('/fanspeed').set(3);
+        }
     }
     else if(number > 3 ){
         outstr = "此電扇目前沒有支援風速 ＝ " + number;
     }
+    else if(speedchange > 0){
+        if(speedchange == 1){
+            // outstr = "風速往上";
+            if(deactivate_db == 0){
+                outstr = "電扇已經停止運轉,開啟電扇並設定為風速1";
+                database.ref('/activate').set('1');
+                database.ref('/deactivate').set('');
+                database.ref('/fanspeed').set(1);
+            }
+            else{
+                if(fanspeed_db < 3){
+                    var fanspeed_ = fanspeed_db;
+                    fanspeed_ = fanspeed_ + 1;
+                    outstr = "電扇設定為風速 " + fanspeed_;
+                    database.ref('/fanspeed').set(fanspeed_);
+                }
+                else{
+                    outstr = "風速已經是最大,建議開啟門窗";
+                }
+            }
+        }
+        else{
+            // outstr = "風速往下";
+            if(deactivate_db == 0){
+                outstr = "電扇已經停止運轉,建議開啟暖氣";
+            }
+            else{
+                if(fanspeed_db > 1){
+                    var fanspeed_ = fanspeed_db;
+                    fanspeed_ = fanspeed_ - 1;
+                    outstr = "電扇設定為風速 " + fanspeed_;
+                    database.ref('/fanspeed').set(fanspeed_);
+                }
+                else{
+                    outstr = "電扇停止運轉";
+                    database.ref('/activate').set('');
+                    database.ref('/deactivate').set('0');
+                }
+            }
+        }
+    }
     else{
         outstr = "電扇已經可以控制了"
-        database.ref().set(params);
+        // database.ref().set(params);
     }
     // Fan Control Logic END ------
 
